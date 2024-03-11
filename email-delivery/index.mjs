8ec9649d2a25343a0aaf011c2e2bd2e4dd5602c8
MNeverOff <mike@neveroff.dev>
2024-03-11 17:17:12 +0000

    Added S3 authentication

--- email-delivery/index.mjs
+++ email-delivery/index.mjs
@@ -2,7 +2,10 @@ import axios from 'axios';
 import stripe from 'stripe';
 import AWS from 'aws-sdk';
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+});
 
 export const handler = async (event) => {
     const body = event.body;
--- file-delivery/index.mjs
+++ file-delivery/index.mjs
@@ -1,7 +1,10 @@
 import Stripe from 'stripe';
 import AWS from 'aws-sdk';
 
-const s3 = new AWS.S3();
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
+});
 
 export const handler = async (event) => {
     const stripe = new Stripe(event.stageVariables.stripe_secret_api_key);
