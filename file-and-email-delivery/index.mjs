aff32874d78f877543fcad8e54a1ac114e8980c5
MNeverOff <mike@neveroff.dev>
2024-03-12 20:54:12 +0000

    removed the aws from node entirely, relying on v3 built in to node 20 in Lambda

--- file-and-email-delivery/index.mjs
+++ file-and-email-delivery/index.mjs
@@ -5,7 +5,7 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 // Specifying the IAM User with S3 access to generate long-lived Signed URLs
 const s3Client = new S3Client({
-    region: "us-east-1",
+    region: process.env.S3_,
     credentials: {
         accessKeyId: process.env.S3_ACCESS_KEY_ID,
         secretAccessKey: process.env.S3_SECRET_ACCESS_KEY
